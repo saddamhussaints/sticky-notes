49302081c1a85103fa902139a39db822da3a0bac
saddamhussaints <saddamhussaints392@gmail.com>
2024-01-10 15:05:31 +0530

    firzt cmt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import StickyNotes from './components/StickyNotes';
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text: "Hello"
+    }
+
+  ]);
+
+  const addNotes = () => {
+    const newNotes = {
+      id: notes.length + 1,
+      text: "New Note"
+    }
+    setNotes([...notes, newNotes])
+  }
+  const DeleteNotes = (id) => {
+    console.log("Dee", id)
+    setNotes((prev) => prev.filter((notes) => notes.id !== id))
+  }
+  const UpdateNotes = (id, newText) => {
+    setNotes((prevNotes) => prevNotes.map((note) => note.id === id ? { ...note, text: newText } : note))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main-header-section'>
+        <h2 className='title'>Sticky Notes</h2>
+        <button className='notes-addbtn' onClick={addNotes}>+</button>
+      </div>
+        <div>
+          {notes.map((note) => <StickyNotes
+            key={note.id}
+            note={note}
+            deleteNotes={DeleteNotes}
+            updateNotes={UpdateNotes}
+          />)}
+      </div>
     </div>
   );
 }
---
+++ src/components/StickyNotes.js
@@ -0,0 +1,66 @@
+import React, { useRef, useState } from 'react'
+import './StickyNotes.css'
+export default function StickyNotes({ note, deleteNotes, updateNotes }) {
+    const [editedValue, setEditedValue] = useState('')
+    const [isedit, setIsEdit] = useState(false)
+    const textareaRef = useRef(null);
+    const stickyNotesRef = useRef(null);
+    const [allowMove, setAllowMove] = useState(false)
+    const [dx, setDx] = useState(0)
+    const [dy, setDy] = useState(0)
+
+    const handleEditClick = () => {
+        setIsEdit(true);
+        setEditedValue(note.text)
+    }
+    const EditText = () => {
+        if (textareaRef.current) {
+            textareaRef.current.focus()
+        }
+    }
+
+    const handleSaveClick = () => {
+        updateNotes(note.id, editedValue);
+        setIsEdit(false)
+    }
+    const handleMouseDown = (e) => {
+        setAllowMove(true)
+        console.log(allowMove + "MouseDown");
+        const dimensions = stickyNotesRef.current.getBoundingClientRect();
+        setDx(e.clientX - dimensions.x)
+        setDy(e.clientY - dimensions.y)
+    }
+    const handleMouseMove = (e) => {
+        if (allowMove) {
+            const x = e.clientX - dx
+            const y = e.clientY - dy
+            stickyNotesRef.current.style.left = x + "px"
+            stickyNotesRef.current.style.top = y + "px"
+        }
+    }
+    const handleMouseUp = () => {
+        setAllowMove(false)
+        console.log(allowMove + "MouseUp")
+
+    }
+    return (
+        <div ref={stickyNotesRef} className='note-item' onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}>
+            <div className='sticky-note-header'>
+                <div></div>
+                <button className='delete-btn' onClick={() => deleteNotes(note.id)}>X</button>
+            </div>
+            {
+                isedit ? <textarea rows={9} cols={10} value={editedValue} ref={textareaRef} onChange={(e) => setEditedValue(e.target.value)} onDoubleClick={handleSaveClick} ></textarea> :
+                    <span className='note' onDoubleClick={handleEditClick}>{note.text}</span>
+            }
+            <div className='btn-container' >
+                {/* <button className='save-btn' onClick={handleSaveClick}>Save</button> */}
+                {isedit && <button className='edit-btn' onClick={EditText}>Edit</button>}
+            </div>
+        </div>
+
+
+    )
+}
